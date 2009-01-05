ec78d92547564c3db120cb98dc66b802dea882da
simon.kagstrom <simon.kagstrom@feb19cf6-d755-11dd-8179-53c65a4d6d95>
2009-01-05 13:10:46 +0000

    Comment and warning

--- Src/gcaudio.c
+++ Src/gcaudio.c
@@ -129,6 +129,8 @@ void PlaySound( int16_t *Buffer, int count )
 	level = IRQ_Disable();
 	for( i = 0; i < count; i++ )
 	{
+		/* FIXME simonk: 8192 is a Frodo artifact. Not sure why this
+		 * is needed */
 		sample = (Buffer[i] & 0xffff) + 8192;
 		dst[mixhead++] = sample | ( sample << 16);
 		if (mixhead == 4000)
