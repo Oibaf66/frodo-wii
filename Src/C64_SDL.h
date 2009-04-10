852c6d546ad3d52a8e967b750d63d7d767f95c66
simon.kagstrom <simon.kagstrom@feb19cf6-d755-11dd-8179-53c65a4d6d95>
2009-04-10 12:01:22 +0000

    Correct hostname

--- Src/C64_SDL.h
+++ Src/C64_SDL.h
@@ -72,7 +72,7 @@ void C64::c64_ctor1(void)
 
 	this->virtual_keyboard = new VirtualKeyboard(real_screen, this->menu_font);
 
-	strncpy(this->server_hostname, "localhost",//"192.168.10.139",//"c64-network.game-host.org",
+	strncpy(this->server_hostname, "c64-network.game-host.org",
 			sizeof(this->server_hostname));
 	this->server_port = 46214;
 	this->network_connection_type = NONE;
