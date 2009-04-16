a45b99e86c0b5f7752df8c7ce9b7d6fd6f10249a
simon.kagstrom <simon.kagstrom@feb19cf6-d755-11dd-8179-53c65a4d6d95>
2009-04-16 19:33:39 +0000

    Added

---
+++ Src/strings.h
@@ -0,0 +1,18 @@
+struct _S_LIST
+{
+	int Elements;
+	char **List;
+	int Sorted;
+};
+typedef struct _S_LIST S_List;
+
+#define S_SORTED 	1
+#define S_UNSORTED 	0
+
+
+/************************************************************/
+extern S_List*	S_CreateList(int Sorted);
+extern int 		S_AddToList(S_List *List, char *String);
+extern void 	S_DestroyList(S_List *List);
+/************************************************************/
+
